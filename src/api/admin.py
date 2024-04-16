1c7bc4659e0aaf76c69e03b90092d900d75c3c3d
Eyasu Betwos <Johb.nca@gmail.com>
2024-04-15 23:37:58 -0700

    admin

--- src/api/admin.py
+++ src/api/admin.py
@@ -23,6 +23,10 @@ def reset():
                 UPDATE global_inventory SET
                 num_green_potions = 0,
                 num_green_ml = 0,
+                num_red_potions = 0,
+                num_red_ml = 0,
+                num_blue_potions = 0,
+                num_blue_ml = 0,
                 gold = 100
             """))
 
